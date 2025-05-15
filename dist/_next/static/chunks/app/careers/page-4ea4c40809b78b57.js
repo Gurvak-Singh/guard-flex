b7974d507d39fe89e931c326de84db5f52d25bb1
Gurvak-Singh <gurvaksingh1998@gmail.com>
2025-05-15 15:54:17 -0600

    feat: build static site with Next.js pages and assets

---
+++ dist/_next/static/chunks/app/careers/page-4ea4c40809b78b57.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[995,301,404,592,216,127,931,469,385,571,207,253],{3774:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=3774)}),_N_E=n.O()}]);
